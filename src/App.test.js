3af0245dbc12140aaf8ef507277cb7f62f30278a
DmitryCurse <dimonil488@gmail.com>
2024-02-13 20:20:30 +0500

    13.02.24

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import SamuraiJSApp from "./App";
-import {createRoot} from "react-dom/client";
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    const root = createRoot(div);
-    root.render(<SamuraiJSApp/>);
-    root.unmount();
-});
